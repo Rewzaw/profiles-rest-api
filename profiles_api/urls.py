a32b03705f4f24e867484a8efff8690e7fbb6932
Reza <reza.unity7@gmail.com>
2020-11-26 23:23:08 +0330

    Added Login Api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,9 +5,11 @@ from . import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
-router.register('profile', views.UserProfileViewSet)  # basename='profile' if we want do override queryset name
+# basename='profile' if we want do override queryset name
+router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello-apiview/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls)),
 ]
